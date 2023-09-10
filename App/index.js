e2a5bd88c7b385e0ef3a875970da5b517da13716
Lissandra Maiara <li_fischer@hotmail.com>
2023-09-10 16:12:03 +0000

    Priemiro aplicativo

---
+++ App/index.js
@@ -0,0 +1,8 @@
+import { registerRootComponent } from 'expo';
+
+import App from './src/App';
+
+import {name as appName} from './app.json';
+
+
+registerRootComponent(App, {initialProps: appName});
